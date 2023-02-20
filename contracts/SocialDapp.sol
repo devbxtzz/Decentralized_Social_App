79a50b9950a59b67433d7e662c281ad96e4e7b55
devbxtzz <batztonnie@gmail.com>
2023-02-20 21:07:03 +0300

    SocialDapp smart contract

---
+++ contracts/SocialDapp.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.1;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+
+
+contract SocialDapp is ERC721("SocialDApp", "TDAPP") {
+    uint256 tokenId;
+    post[] public posts;
+
+    struct post{
+        string name;
+        string description;
+        uint256 upvotes;
+        string[] comments;
+        address fromAddress;
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        bytes memory dataURI = abi.encodePacked(
+            '{',
+                '"name":', '"', posts[_tokenId].name, '",'  '"description":' , '"',  posts[_tokenId].description, '"', ',' ,
+            
+            '"attributes":', '[', '{', '"trait_type":', '"Upvotes",' , '"value":', Strings.toString(posts[_tokenId].upvotes), '}', ']' , '}'
+        );
+
+        return string(
+            abi.encodePacked(
+                "data:application/json;base64,",
+                Base64.encode(dataURI)
+            )
+        );
+    }
+
+    function writepost(string memory prefName, string memory prefDesc) public {
+        _safeMint(msg.sender, tokenId);
+        posts.push(post({
+            name: prefName,
+            description: prefDesc,
+            upvotes: 0,
+            comments: new string[](0),
+            fromAddress: msg.sender
+        }));
+        tokenId = tokenId + 1;
+    }
+
+    function upvote(uint postIndex) public {
+        posts[postIndex].upvotes += 1;
+    }
+
+    function addComment(uint256 postIndex, string memory prefComments) public {
+        posts[postIndex].comments.push(prefComments);
+    }
+
+    function getAllposts() public view returns(post[] memory) {
+        return posts;
+    }
+}
